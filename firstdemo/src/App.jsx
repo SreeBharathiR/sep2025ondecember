0856bccbcb4f1e877662af6c08ca9de41b5ad045
SreeBharathiR <bharathisree130@gmail.com>
2025-12-12 16:15:43 +0530

    UseReducer - class 2

--- firstdemo/src/App.jsx
+++ firstdemo/src/App.jsx
@@ -41,22 +41,60 @@ function App() {
 
   const reducerFunc = (currentState, action) => {
     switch (action.type) {
-      case "ADD":
-        return [
-          ...currentState,
-          {
-            id: currentState.length + 1,
-            name: action.payload.name,
-            age: action.payload.age,
-            department: action.payload.department,
-            marks: {
-              maths: action.payload.marks.math,
-              science: action.payload.marks.sci,
-              english: action.payload.marks.eng,
-            },
+      case "ADD": {
+        let newdata = {
+          id: currentState.length + 1,
+          name: action.payload.name,
+          age: action.payload.age,
+          department: action.payload.department,
+          marks: {
+            maths: action.payload.marks.math,
+            science: action.payload.marks.sci,
+            english: action.payload.marks.eng,
           },
-        ];
-      case "DELETE":
+        };
+        return [...currentState, newdata];
+      }
+      case "DELETE": {
+        const selected = currentState.filter(
+          (item) => item.id !== Number(action.payload)
+        );
+        console.log("reducer");
+        console.log(selected);
+        return selected;
+      }
+      case "UPDATEUSER": {
+        // const selected = currentState.filter(
+        //   (item) => item.id !== Number(action.payload.id)
+        // );
+        // let updateddata = {
+        //   id: action.payload.id,
+        //   name: action.payload.name,
+        //   age: action.payload.age,
+        //   department: action.payload.department,
+        //   marks: {
+        //     maths: action.payload.marks.math,
+        //     science: action.payload.marks.sci,
+        //     english: action.payload.marks.eng,
+        //   },
+        // };
+        let update = currentState.map((item) =>
+          item.id === Number(action.payload.id)
+            ? {
+                id: action.payload.id,
+                name: action.payload.name,
+                age: action.payload.age,
+                department: action.payload.department,
+                marks: {
+                  maths: action.payload.marks.math,
+                  science: action.payload.marks.sci,
+                  english: action.payload.marks.eng,
+                },
+              }
+            : item
+        );
+        return update;
+      }
       default:
         return currentState;
     }
@@ -66,6 +104,10 @@ function App() {
   const [age, setAge] = useState(0);
   const [dept, setDept] = useState("");
   const [marks, setMarks] = useState({ math: 0, sci: 0, eng: 0 });
+  const [selectedStudent, setSelectedStudent] = useState(null);
+  const [selectedStudentDetails, setSelectedStudentDetails] = useState(null);
+  const { math, sci, eng } = marks;
+  console.log("Math ", math);
   console.log(name);
   console.log(age);
   console.log(dept);
@@ -81,19 +123,113 @@ function App() {
       },
     });
   };
-  console.log(state);
+
+  const deleteFunc = () => {
+    dispatch({
+      type: "DELETE",
+      payload: selectedStudent,
+    });
+  };
+
+  const updateFunc = () => {
+    dispatch({
+      type: "UPDATEUSER",
+      payload: selectedStudentDetails,
+    });
+  };
+  console.log("selectedStudent ", selectedStudent);
+  // const getValue = (field) => {
+  //   const selected = state.find((item) => item.id === Number(selectedStudent));
+  //   return selected[field];
+  // };
   return (
     <>
       <ol>
         {" "}
+        {/* <label name={"student"}></label> */}
         {state.map((val) => (
           <li key={val.id}>
+            <input
+              type="radio"
+              name={"student"}
+              value={val.id}
+              id={val.id}
+              onChange={(e) => {
+                setSelectedStudent(e.target.value);
+                setSelectedStudentDetails(val);
+              }}
+            />
             <p>{val.name}</p>
             <p>{val.age}</p>
             <p>{val.department}</p>
           </li>
         ))}
       </ol>
+      <button onClick={deleteFunc}>Delete</button>
+      <div>
+        <p>
+          <strong>Update Student Details:</strong>
+        </p>
+        <br></br>
+        Name:
+        <input
+          type="text"
+          onChange={(e) =>
+            setSelectedStudentDetails({
+              ...selectedStudentDetails,
+              name: e.target.value,
+            })
+          }
+          // onChange={(e) => setName(e.target.value)}
+          value={selectedStudentDetails?.name || ""}
+        ></input>
+        <br></br>
+        Age:
+        <input
+          type="number"
+          onChange={(e) =>
+            setSelectedStudentDetails({
+              ...selectedStudentDetails,
+              age: e.target.value,
+            })
+          }
+          value={selectedStudentDetails?.age || ""}
+        ></input>
+        <br></br>
+        Dept:
+        <input
+          type="text"
+          onChange={(e) =>
+            setSelectedStudentDetails({
+              ...selectedStudentDetails,
+              department: e.target.value,
+            })
+          }
+          value={selectedStudentDetails?.department || ""}
+        ></input>
+        <br></br>
+        Marks:
+        <br></br>
+        Maths:
+        <input
+          type="number"
+          onChange={(e) => setMarks({ ...marks, math: e.target.value })}
+        ></input>{" "}
+        <br></br>Science
+        <input
+          type="number"
+          onChange={(e) => setMarks({ ...marks, sci: e.target.value })}
+        ></input>{" "}
+        <br></br>English
+        <input
+          type="number"
+          onChange={(e) => setMarks({ ...marks, eng: e.target.value })}
+        ></input>
+        <br></br>
+        <button onClick={updateFunc}>Update</button>
+      </div>
+      <br></br>
+      <p>Create new student</p>
       Name:
       <input type="text" onChange={(e) => setName(e.target.value)}></input>
       <br></br>
