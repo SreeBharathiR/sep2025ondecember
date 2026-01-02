27357538d7d7ceb338c20cdba8c1dcb9a407aa86
SreeBharathiR <bharathisree130@gmail.com>
2026-01-02 16:00:22 +0530

    Backend - server lister, CRUD operations & status code

---
+++ Backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+// locate a location , listen- server
+
+app.listen(5000, () => {
+  console.log("server is running on port 5000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our server!");
+});
